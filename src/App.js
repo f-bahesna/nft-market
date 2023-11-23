ebeb8d176426bb48eb8797d8fb811f7d308bf599
Lianum Frada Bahesna <fbahezna@gmail.com>
2023-11-23 09:22:11 +0700

    enhance: connect to metamask and change account feature

--- src/App.js
+++ src/App.js
@@ -15,9 +15,32 @@ import config from './config.json';
 
 function App() {
 
+    const [account, setAccount] = useState(null)
+
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+        // change account
+        window.ethereum.on('accountsChanged', async () => {
+            const accounts = await window.ethereum.request(
+                {
+                    method: 'eth_requestAccounts'
+                }
+            );
+
+            const account = ethers.utils.getAddress(accounts[0])
+            setAccount(account);
+        })
+    }
+
+    useEffect(() => {
+        loadBlockchainData()
+    }, [])
+
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
       <div className='cards__section'>
 
         <h3>Welcome to Millow</h3>
